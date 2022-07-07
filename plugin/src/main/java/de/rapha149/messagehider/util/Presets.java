31852a01d0d3a8254e72b5cbb26120469c151d87
Rapha149 <raphael@raphael-jantzen.de>
2022-07-07 21:43:37 +0200

    Fix error with presets.

--- plugin/src/main/java/de/rapha149/messagehider/util/Presets.java
+++ plugin/src/main/java/de/rapha149/messagehider/util/Presets.java
@@ -5,6 +5,7 @@ import de.rapha149.messagehider.util.Config.FilterData;
 import de.rapha149.messagehider.util.Config.FilterData.MessageData;
 import de.rapha149.messagehider.util.Config.FilterData.MessageData.JsonData;
 import de.rapha149.messagehider.util.Config.FilterData.TargetsData;
+import de.rapha149.messagehider.version.Replacement;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -20,31 +21,31 @@ public class Presets {
 
     static {
         GAMEMODE_CHANGE = new FilterData("gamemode_change", new MessageData(loadJson("gamemode_change.json"),
-                null, false, null, true, new JsonData(true, 1)),
+                new Replacement(), false, null, true, new JsonData(true, 1)),
                 false, false, false, null, Arrays.asList());
 
         if (Updates.isBukkitVersionAboveOrEqualTo("1.16")) {
             ONLY_SELF_COMMANDS = new FilterData("only_self_commands", new MessageData(loadJson("only_self_commands.json"),
-                    null, false, null, true, new JsonData(true, 1)),
+                    new Replacement(), false, null, true, new JsonData(true, 1)),
                     true, false, false, null, Arrays.asList());
 
             CONSOLE_COMMANDS = new FilterData("console_commands", new MessageData(loadJson("console_commands.json"),
-                    null, false, null, true, new JsonData(true, 1)),
+                    new Replacement(), false, null, true, new JsonData(true, 1)),
                     false, false, false, null, Arrays.asList(),
                     new TargetsData(Arrays.asList(Util.ZERO_UUID.toString()), Arrays.asList(), Arrays.asList(), Arrays.asList()));
         }
 
         if (Updates.isBukkitVersionAboveOrEqualTo("1.13")) {
             IDLE_TIMEOUT = new FilterData("idle_timeout", new MessageData(loadJson("idle_timeout_1.13.json"),
-                    null, false, null, true, new JsonData(true, 3)),
+                    new Replacement(), false, null, true, new JsonData(true, 3)),
                     true, false, false, null, Arrays.asList());
         } else if (Updates.isBukkitVersionAboveOrEqualTo("1.8")) {
             IDLE_TIMEOUT = new FilterData("idle_timeout", new MessageData(loadJson("idle_timeout_1.8.json"),
-                    null, false, null, true, new JsonData(true, 3)),
+                    new Replacement(), false, null, true, new JsonData(true, 3)),
                     true, false, false, null, Arrays.asList());
         } else {
             IDLE_TIMEOUT = new FilterData("idle_timeout", new MessageData("commands.setidletimeout.success",
-                    null, false, null, false, new JsonData(false, 0)),
+                    new Replacement(), false, null, false, new JsonData(false, 0)),
                     false, false, false, null, Arrays.asList());
         }
     }
